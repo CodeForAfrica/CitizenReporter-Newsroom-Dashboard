929e822c1111d4ad68d64345079904233c9995d3
Edwin Kato <edwin.kato@andela.com>
2017-12-11 11:41:26 +0300

    Update the .env.example file

--- dashboard/src/set-env.ts
+++ dashboard/src/set-env.ts
@@ -1,13 +1,9 @@
 import { writeFile } from 'fs';
 import { argv } from 'yargs';
 
-// This is good for local dev environments, when it's better to
-// store a projects environment variables in a .gitignore'd file
 require('dotenv').config();
 
-// Would be passed to script like this:
-// `ts-node set-env.ts --environment=dev`
-// we get it from yargs's argv object
+// The variable will be passed to the script like this: `ts-node set-env.ts --environment=dev`
 const environment = argv.environment;
 const isProd = environment === 'prod';
 
