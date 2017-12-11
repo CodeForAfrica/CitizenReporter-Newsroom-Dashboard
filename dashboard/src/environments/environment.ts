b6ba3c1ae20801751305ad298071d04d9421ef25
Edwin Kato <edwin.kato@andela.com>
2017-12-11 12:03:51 +0300

    Add .env to .gitignore

--- dashboard/src/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-// This file's contents will be overwritten by the values in the .env file during build.
-export const environment = {
-  production: true,
-  APP_ID: 11235813,
-  MASTER_KEY: 11235813,
-  SERVER_URL: 'https://creporter-server.herokuapp.com/parse',
-  FLURRY_APP_ANALYTICS_TOKEN: 'eyJhbGciOiJIUzI1NiIsImtpZCI6ImZsdXJyeS56dXVsLnByb2Qua2V5c3RvcmUua2V5LjIifQ.eyJpc3MiOiJodHRwczovL3p1dWwuZmx1cnJ5LmNvbTo0NDMvdG9rZW4iLCJpYXQiOjE1MTE5NjA1NDQsImV4cCI6MzMwNjg4NjkzNDQsInN1YiI6IjQwOTc4NyIsImF1ZCI6IjQiLCJ0eXBlIjo0LCJqdGkiOiIyODE1In0.tjwrc23z1-kc3jQXR8bLzUYGvm3De6ms4VZWYatFSnU'
-};
